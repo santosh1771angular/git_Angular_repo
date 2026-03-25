4e76d290c75072a381e18ec683562863b3b068be
Santosh Kumar <santosh1771@outlook.com>
2026-03-25 10:53:11 +0530

    Added feature1 changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignalsexapleComponent } from './signalsexaple/signalsexaple.component';
+import { TrackbyComponent } from './trackby/trackby.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignalsexapleComponent],
+  imports: [RouterOutlet, SignalsexapleComponent,
+    TrackbyComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
